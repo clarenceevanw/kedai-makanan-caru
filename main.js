af1cdd8047a9ef83fbe1725bf9f3843b47ea6df6
clarenceevanw <clarenceevan0907@gmail.com>
2024-08-07 00:53:48 +0700

    Membuat website katering Caru

---
+++ main.js
@@ -0,0 +1,12 @@
+const navbarNav = document.querySelector(".navbar-nav");
+const hamburgerMenu = document.querySelector("#hamburger-menu");
+
+hamburgerMenu.addEventListener('click', ()=> {
+    navbarNav.classList.toggle('active');
+});
+
+document.addEventListener('click', (e) =>{
+    if(!hamburgerMenu.contains(e.target) && !navbarNav.contains(e.target)){
+        navbarNav.classList.remove('active');
+    };
+});
